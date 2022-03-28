75c42ce231d4eb8f31fc9a7989554ec6a4983394
k3shav17 <keshavrao0489@gmail.com>
2022-03-28 17:00:05 +0530

    finding a book by name with case insensitive search

--- src/main/java/com/book/manager/dao/BookRepository.java
+++ src/main/java/com/book/manager/dao/BookRepository.java
@@ -3,13 +3,15 @@ package com.book.manager.dao;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.book.manager.entity.Book;
 
-public interface BookRepository extends MongoRepository<Book, String>{
-
-	Optional<Book> findBookByBookName(String bookName);
-
-//	Optional<Book> findBookById(String id);
-
+public interface BookRepository extends MongoRepository<Book, String> {
+	
+	Optional<Book> findBookById(String id);
+	
+	@Query(value = "{'bookName': {$regex : ?0, $options: 'i'}}")
+	Optional<Book> findBookByName(String bookName);	
+	
 }
